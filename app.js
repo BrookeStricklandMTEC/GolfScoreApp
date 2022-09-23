2f6cb87f137f70ec99f9ad7e6a7fad45afbb6bdc
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2022-09-23 09:26:44 -0600

    basics and tips added

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 // Golf 
 
 
+//  Functions I wil need
 
+// addList
+// removeList
+// addTodo
+// removeTodo
+// markTodoAsCompleted
+// removeAllTodosCompleted
+
+
+// Player Rows
+
+    // class Player {
+    // constructor(name, id = getNextId(), scores = []) {
+    //     this.name = name;
+    //     this.id = id;
+    //     this.scores = scores;
+    //   }
+// }
+
+
+// Notification for Finish
+    // class Player {
+    //     constructor(name, id = getNextId(), scores = []) {
+    //       this.name = name;
+    //       this.id = id;
+    //       this.scores = scores;
+    //     }
+//   }
 
 
 
