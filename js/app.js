6c6f8765719e258f2b53e67522b34c954ce15438
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2022-10-26 09:53:49 -0600

    only needing to add toasts now!

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // Golf 
 
-// import scoring from '/scoring.js' 
+let currentCourse = null; 
+let teeType = null; 
+
 
 function getAvailableCourses() {
     fetch('https://golf-courses-api.herokuapp.com/courses/',
@@ -35,7 +37,7 @@ courseDiv.addEventListener('change', function () {
                 return res.json()
             })
             .then(data => {
-
+                currentCourse = data.data; 
                 renderTee(data.data.holes[0].teeBoxes);
             })
             .catch(error => {
@@ -48,6 +50,7 @@ courseDiv.addEventListener('change', function () {
 // courseSelect();
 let i = 0
 let newPlayerCounter = i++
+
 $('body').ready(function () {
     $('#newPlayerInput').keyup(function (a) {
         if (a.key == "Enter" && newPlayerCounter < 4) {
@@ -60,7 +63,6 @@ $('body').ready(function () {
             `
             )
         }
-        let newPlayer = $('#newPlayerInput').val()
     })
 })
 
@@ -76,53 +78,72 @@ function renderCourses(courses) {
     });
     document.getElementById('course-select').innerHTML = courseOptionsHtml;
 }
-// Tee Box Select && Golf Terms
+
 function renderTee(teeBox) {
     let teeBoxSelectHtml = '';
-    teeBox.forEach((teeBox, index) => {
+    teeBox.forEach((teeBox) => {
         teeBoxSelectHtml +=
         `
-        <option value="${index}">${teeBox.teeType.toUpperCase()}</option>
+        <option value="${teeBox.teeType}">${teeBox.teeType.toUpperCase()}</option>
         <option value="none" selected disabled hidden> Select TeeBox </option>
         `
-
     });
 
-    // let yardSelectHtml = '';
-    // yardage.forEach((yard) => {
-    //     yardSelectHtml +=
-    //     `
-    //     <span class="golfTerms"> Yardage  ${teeBox.yards.toUpperCase()} </span>
-    //     `
-    // });
-
-    // let parSelectHtml = '';
-    // par.forEach((par) => {
-    //     parSelectHtml +=
-    //     `
-    //     <span class="golfTerms"> Yardage  ${teeBox.par.toUpperCase()} </span>
-    //     `
-    // });
-
-    // let hcpSelectHtml = '';
-    // hcp.forEach((hcp) => {
-    //     hcpSelectHtml +=
-    //     `
-    //     <span class="golfTerms"> Yardage  ${teeBox.hcp.toUpperCase()} </span>
-    //     `
-    // });
-
     document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
-    // document.getElementsByClassName('golfTerms').innerHTML = yardSelectHtml , parSelectHtml , hcpSelectHtml; 
 }
 
 
 
-// Notification for Finish
-    // class Player {
-    //     constructor(name, id = getNextId(), scores = []) {
-    //       this.name = name;
-    //       this.id = id;
-    //       this.scores = scores;
-    //     }
-//   }
+document.getElementById('tee-box-select').addEventListener('change', function(event){
+    teeType = event.target.value;
+    renderYardage(); 
+    renderPar();
+    renderHCP();
+});
+
+function renderYardage(){
+    const totalYardage = getTotalYardage();
+    const yardageElement = document.querySelector('.yardage');
+
+    yardageElement.textContent = totalYardage;
+    
+}
+
+function getTotalYardage() {
+    const holes = currentCourse.holes;
+    const yards = holes.map(holeItem => holeItem.teeBoxes.find(teeBoxItem => teeBoxItem.teeType === teeType).yards);
+
+    return yards.reduce((totalYards, yardItem) => totalYards + yardItem, 0)    
+}
+
+
+function renderPar(){
+    const totalPar = getTotalPar();
+    const ParElement = document.querySelector('.par');
+
+    ParElement.textContent = totalPar;
+    
+}
+
+function getTotalPar() {
+    const holes = currentCourse.holes;
+    const par = holes.map(holeItem => holeItem.teeBoxes.find(teeBoxItem => teeBoxItem.teeType === teeType).par);
+
+    return par.reduce((totalPar, parItem) => totalPar + parItem, 0)    
+}
+
+function renderHCP(){
+    const totalHCP = getTotalHCP();
+    const HCPElement = document.querySelector('.hcp');
+
+    HCPElement.textContent = totalHCP;
+    
+}
+
+function getTotalHCP() {
+    const holes = currentCourse.holes;
+    const hcp = holes.map(holeItem => holeItem.teeBoxes.find(teeBoxItem => teeBoxItem.teeType === teeType).hcp);
+
+    return hcp.reduce((totalHCP, HCPItem) => totalHCP + HCPItem, 0)    
+}
+
--- js/scoring.js
+++ js/scoring.js
@@ -1,9 +1,4 @@
 
-playerOne = [] 
-playerTwo = [] 
-playerThree = [] 
-playerFour= [] 
-
 // only allows numerical entries 
 function validate(evt) {
     let theEvent = evt || window.event;
@@ -22,63 +17,117 @@ function validate(evt) {
     }
 }
 
-// if a score is entered, update current score for that player, store it in player array. 
 let a = 0
-let newRowCounter = a++
+let rowOneCounter = a++
+let rowTwoCounter = a++
+let rowThreeCounter = a++
+let rowFourCounter = a++
 
 $(document).ready(function(){
     $('#playerOneRow').keyup(function(f){
-        if (f.key == "Enter" && newRowCounter < 17){
+        if (f.key == "Enter" && rowOneCounter < 17){
          $('#playerOneRow').append(
          `
          <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerOneScore">
          `
          )
-         newRowCounter++
+         let playerOneScore = document.querySelectorAll('#playerOneScore'), sumVal = 0; 
+         for (let i = 0; i < playerOneScore.length; i++){
+            sumVal += parseInt(playerOneScore[i].value || 0);
+         } 
+         let playerOneCurrent2 = document.querySelectorAll('#playerOneCurrent');
+         playerOneCurrent2 +=
+         `
+         <div id="playerOneCurrent"> ${sumVal} </div>
+         `
+            rowOneCounter++
+            document.getElementById('playerOneCurrent').innerHTML = sumVal;
+            console.log(sumVal); 
         }
     });
-    $('#playerTwoRow').keyup(function(f){
-        if (f.key == "Enter" && newRowCounter < 17){
-         $('#playerTwoRow').append(
-         `
-         <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerOneScore">
-         `
-         )
-         newRowCounter++
-        }
-    });
-    $('#playerThreeRow').keyup(function(f){
-        if (f.key == "Enter" && newRowCounter < 17){
-         $('#playerThreeRow').append(
-         `
-         <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerOneScore">
-         `
-         )
-         newRowCounter++
-        }
-    });
-    $('#playerFourRow').keyup(function(f){
-        if (f.key == "Enter" && newRowCounter < 17){
-         $('#playerFourRow').append(
-         `
-         <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerOneScore">
-         `
-         )
-         newRowCounter++
-        }
-    });
-
 });
 
+$(document).ready(function(){
+$('#playerTwoRow').keyup(function(f){
+        if (f.key == "Enter" && rowTwoCounter < 18){
+        $('#playerTwoRow').append(
+        `
+        <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerTwoScore">
+        `
+        )
+        let playerTwoScore = document.querySelectorAll('#playerTwoScore'), sumVal1 = 0; 
+        for (let i = 0; i < playerTwoScore.length; i++){
+            sumVal1 += parseInt(playerTwoScore[i].value || 0);
+        } 
+        let playerTwoCurrent2 = document.querySelectorAll('#playerTwoCurrent');
+        playerTwoCurrent2 +=
+        `
+        <div id="playerTwoCurrent"> ${sumVal1} </div>
+        `
+        rowTwoCounter++
+        document.getElementById('playerTwoCurrent').innerHTML = sumVal1;
+        console.log(sumVal1); 
+        }
+    });
+});
 
-// console.log(scoring)
+$(document).ready(function(){
+$('#playerThreeRow').keyup(function(f){
+        if (f.key == "Enter" && rowThreeCounter < 19){
+        $('#playerThreeRow').append(
+        `
+        <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerThreeScore">
+        `
+        )
+        let playerThreeScore = document.querySelectorAll('#playerThreeScore'), sumVal2 = 0; 
+        for (let i = 0; i < playerThreeScore.length; i++){
+            sumVal2 += parseInt(playerThreeScore[i].value || 0);
+        } 
+        let playerThreeCurrent2 = document.querySelectorAll('#playerThreeCurrent');
+        playerThreeCurrent2 +=
+        `
+        <div id="playerThreeCurrent"> ${sumVal2} </div>
+        `
+        document.getElementById('playerThreeCurrent').innerHTML = sumVal2;
+        rowThreeCounter++
+        }
+    });
+});
+
+$(document).ready(function(){
+$('#playerFourRow').keyup(function(f){
+        if (f.key == "Enter" && rowFourCounter < 20){
+        $('#playerFourRow').append(
+        `
+        <input type='text' onkeypress='validate(event)' class="row-sml-score" maxlength="4" id="playerFourScore">
+        `
+        )
+        let playerFourScore = document.querySelectorAll('#playerFourScore'), sumVal3 = 0; 
+        for (let i = 0; i < playerFourScore.length; i++){
+            sumVal3 += parseInt(playerFourScore[i].value || 0);
+        } 
+        let playerFourCurrent2 = document.querySelectorAll('#playerFourCurrent');
+        playerFourCurrent2 +=
+        `
+        <div id="playerFourCurrent"> ${sumVal3} </div>
+        `
+        rowOneCounter++
+        document.getElementById('playerFourCurrent').innerHTML = sumVal3;
+        }
+    }); 
+});
 
 // Notification for Finish
-    // class Player {
-    //     constructor(name, id = getNextId(), scores = []) {
-    //       this.name = name;
-    //       this.id = id;
-    //       this.scores = scores;
-    //     }
-//   }
+
+// function toasts (){
+
+// if (){
+
+//     toastr.success('Congrats, you are (L)PGA Tour material');
+
+// }
+
+// }
+
+
 
